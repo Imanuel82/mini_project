d69a0f5d8e63d5ac6187cec32a017e5f5fdc4137
Imanuel82 <78832048+Imanuel82@users.noreply.github.com>
2022-06-16 14:24:31 +0700

    Add files via upload

---
+++ Mini_Project/all/manageContent.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda Konten</title>
+</head>
+<body>
+    <a href="crudOlahraga.php">Manage Olahraga</a>
+    <br>
+    <a href="crudDtlOlahraga.php">Manage Detail Olahraga</a>
+    <br>
+    <a href="crudVideo.php">Manage Video</a>
+    <br>
+    <a href="crudInstruktur.php">Manage Instruktur</a>
+    <br><br><br>
+    <a href="dashboardAdmin.php">Admin Dashboard</a>
+</body>
+</html>
